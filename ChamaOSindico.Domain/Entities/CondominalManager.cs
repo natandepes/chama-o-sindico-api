dbc8cf33a360f454f135aa190f76196783a7f549
TheAbraaonix <carlosabraao@hotmail.com.br>
2025-04-04 20:53:32 -0300

    criando as entidades de domínio (parte 2)

---
+++ ChamaOSindico.Domain/Entities/CondominalManager.cs
@@ -0,0 +1,8 @@
+﻿namespace ChamaOSindico.Domain.Entities
+{
+    public class CondominalManager : Person
+    {
+        public bool IsResident { get; set; }
+        public double Salary { get; set; }
+    }
+}
--- ChamaOSindico.Domain/Entities/Document.cs
+++ ChamaOSindico.Domain/Entities/Document.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChamaOSindico.Domain.Entities
 {
-    public class CondominalDocument
+    public class Document
     {
         public string? Id { get; set; }
         public string? Name { get; set; }
