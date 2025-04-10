f7f3b404e2ab50f923252fef2c34e9ee26fd10d4
TheAbraaonix <carlosabraao@hotmail.com.br>
2025-04-09 23:04:41 -0300

    removendol mudanças do swagger

--- ChamaOSindico.WebAPI/Program.cs
+++ ChamaOSindico.WebAPI/Program.cs
@@ -46,10 +46,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(c =>
-    {
-        c.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0;
-    });
+    app.UseSwagger();
     app.UseSwaggerUI();
 }
 
