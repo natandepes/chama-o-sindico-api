1a02a176e1d71aa9ecb1e4119c8c0c86418984ed
Drakemaner <guilhermejenner10@gmail.com>
2025-04-11 15:16:20 -0300

    Excluindo Serviços não usados novamente

--- ChamaOSindico.IoC/DependencyInjection.cs
+++ ChamaOSindico.IoC/DependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using ChamaOSindico.Application.Auth;
 using ChamaOSindico.Application.Interfaces;
-using ChamaOSindico.Application.Service;
 using ChamaOSindico.Application.Services;
 using ChamaOSindico.Domain.Interfaces;
 using ChamaOSindico.Application.Auth;
@@ -63,36 +62,8 @@ namespace ChamaOSindico.IoC
             // Register Services
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<JwtService>();
-            services.AddScoped<IVehicleService, VehicleService>();
-            services.AddScoped<IComplaintService, ComplaintService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IAreaService, AreaService>();
-
-            // Register Auth + JWT middleware
-            services.AddJwtAuthentication(configuration);
-
-            return services;
-        }
-
-        private static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = configuration["Jwt:Issuer"],
-                        ValidAudience = configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
-                    };
-                });
-            services.AddScoped<IAuthService, AuthService>();
-            services.AddScoped<JwtService>();
-            services.AddScoped<IUserService, UserService>();
 
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
                 AppDomain.CurrentDomain.Load("ChamaOSindico.Application"))
