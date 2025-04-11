1a9e9116a8145f159a827d3b29e80bb979cece4f
TheAbraaonix <carlosabraao@hotmail.com.br>
2025-04-11 17:25:03 -0300

    implementando o restante do crud de residente

--- ChamaOSindico.Domain/Interfaces/IVehicleRepository.cs
+++ ChamaOSindico.Domain/Interfaces/IVehicleRepository.cs
@@ -6,11 +6,11 @@ namespace ChamaOSindico.Domain.Interfaces
 
         public interface IVehicleRepository
         {
-                Task<List<Vehicle>> GetAllVehiclesAsync();
-                Task<List<Vehicle>> GetAllVehiclesByUserIdAsync(string userId);
-                Task<Vehicle> GetVehicleByIdAsync(string idVehicle);
-                Task CreateVehicleAsync(Vehicle vehicle);
-                Task UpdateVehicleAsync(string idVehicle, Vehicle vehicle);
-                Task DeleteVehicleAsync(string idVehicle);
+            Task<List<Vehicle>> GetAllVehiclesAsync();
+            Task<List<Vehicle>> GetAllVehiclesByUserIdAsync(string userId);
+            Task<Vehicle> GetVehicleByIdAsync(string idVehicle);
+            Task CreateVehicleAsync(Vehicle vehicle);
+            Task UpdateVehicleAsync(string idVehicle, Vehicle vehicle);
+            Task DeleteVehicleAsync(string idVehicle);
         }
 }
